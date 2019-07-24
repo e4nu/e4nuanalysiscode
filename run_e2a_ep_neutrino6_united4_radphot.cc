087a914cde0089ec12ae4c6d6d4beda7e6808325
adishka <46301638+adishka@users.noreply.github.com>
2019-07-23 23:45:30 -0500

    adding original files

---
+++ run_e2a_ep_neutrino6_united4_radphot.cc
@@ -0,0 +1,26 @@
+#include "e2a_ep_neutrino6_united4_radphot.C"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+
+  if( argc < 3 ){
+    cout<<"Please specify the target (3He, 56Fe, C12, 4He) and the beam energy (2261 or 4461)"<<endl;
+    cout<<"================= Usage ==============="<<endl;
+    cout<<"./run_e2a_ep_neutrino6_united4_radphot.cc target beam_energy"<<endl;
+    exit(1);
+  }
+
+
+std::string target = argv[1];
+std::string beam_en = argv[2];
+
+ e2a_ep_neutrino6_united4_radphot  t(target,beam_en);
+  t.Loop();
+
+  return 0;
+}

20aeee38a373e5b00b644da7fd27fcade34ee77c
hauenst <hauenst@jlab.org>
2019-10-09 16:27:44 -0400

    Implemented Number of Rotations for Subtractions as a parameter for Genie_analysis

--- run_genie_analysis.C
+++ run_genie_analysis.C
@@ -10,10 +10,11 @@ int main(int argc, char **argv)
 {
 
 
-  if( argc < 4 ){
-    std::cout<<"Please specify the target (3He, 56Fe, C12, 4He), the beam energy (2261 or 4461) and the data type (CLAS=0 or simulation=1)"<<std::endl;
+  if( argc < 5 ){
+    std::cout<<"Please specify the target (3He, 56Fe, C12, 4He), the beam energy (2261 or 4461), the data type (CLAS=0 or simulation=1) "<<std::endl;
+    std::cout<<"and the number of rotations"<<std::endl;
     std::cout<<"================= Usage ==============="<<std::endl;
-    std::cout<<"./genie_analysis target beam_energy 0/1"<<std::endl;
+    std::cout<<"./genie_analysis target beam_energy 0/1 #rot"<<std::endl;
     exit(1);
   }
 
@@ -21,14 +22,19 @@ int main(int argc, char **argv)
   std::string target  = argv[1];
   std::string beam_en = argv[2];
   int choice = atoi(argv[3]);
+  int rotations = atoi(argv[4]);
 
   if (choice != 1 && choice != 0) {
     std::cout << "Unknown option for parameter 3. It should be either 0 or 1. The given value is " << choice << std::endl;
     return 0;
   }
 
+  if (rotations <= 0) {
+    std::cout << "Not a valid number for the number of rotations (parameter 4). The given value is " << rotations << std::endl;
+    return 0;
+  }
 
-  genie_analysis  t(target,beam_en);
+  genie_analysis  t(target,beam_en, rotations);
   t.Loop(choice);
 
 

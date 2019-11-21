a22432761c28aa0104abdf59920676cfc8baa919
afropapp13 <afropapp@yahoo.gr>
2019-11-21 15:34:40 -0600

    updating downloading script, expanding axes in 2D plot

--- download_files.sh
+++ download_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 xB=NoxBCut
-#xB=NoxBCut
+#xB=xBCut
 
 # Genie Samples
 
+echo "/home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_hA2018_Final_NoRadCorr_Plots_FSI_em.root"
 scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_hA2018_Final_NoRadCorr_Plots_FSI_em.root
 scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/4He_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
 scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/12C_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
@@ -15,10 +16,10 @@ scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_56Fe_4461_neutrino6
 
 # Data Samples
 
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/4He_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/12C_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/56Fe_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/4He_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/12C_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
-scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/56Fe_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/Data_Final/$xB/12C_1_161_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/Data_Final/$xB/4He_2_261_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/Data_Final/$xB/12C_2_261_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/Data_Final/$xB/56Fe_2_261_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/Data_Final/$xB/4He_4_461_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/Data_Final/$xB/12C_4_461_Data_Final_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/Data_Final/$xB/56Fe_4_461_Data_Final_Plots_FSI_em.root

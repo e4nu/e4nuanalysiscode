b1297e449b328b17894c95e2c4b9bb9880108633
afropapp13 <afropapp@yahoo.gr>
2019-11-21 08:41:10 -0600

    adding script to download all files from jlab machines

---
+++ download_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+xB=NoxBCut
+#xB=NoxBCut
+
+# Genie Samples
+
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/4He_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/12C_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/56Fe_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_4He_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/4He_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_C12_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/12C_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/genie_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/56Fe_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+
+# Data Samples
+
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/4He_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/12C_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/2_261/hA2018_Final_NoRadCorr/$xB/56Fe_2_261_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_4He_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/4He_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_C12_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/12C_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root
+scp apapadop@ftp.jlab.org:/u/home/apapadop/e4nu/data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test.root /home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles/4_461/hA2018_Final_NoRadCorr/$xB/56Fe_4_461_hA2018_Final_NoRadCorr_Plots_FSI_em.root

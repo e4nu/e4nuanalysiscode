9b53a3613454fce7985a0036aa2e8e010ff721da
afropapp13 <afropapp@yahoo.gr>
2020-01-17 17:41:43 -0600

    exporting genie path name

--- download_files.sh
+++ download_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xB=NoxBCut
-#xB=xBCut
+#xB=NoxBCut
+xB=xBCut
 
 JLabPath=/u/home/apapadop/e4nu
 LocalPath=/home/afroditi/Dropbox/PhD/myCode/30th_Refactorization/myFiles
@@ -11,11 +11,11 @@ JLabAccount=apapadop@ftp.jlab.org
 DataExtension=Data_Final
 GENIEExtension=hA2018_Final_NoRadCorr_LFGM
 
-# Genie Samples
+##### Genie Samples
 
 echo "$LocalPath/1_161/${GENIEExtension}/$xB/12C_1_161_${GENIEExtension}_Plots_FSI_em.root"
 
-scp $JLabAccount:$JLabPath/genie_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root $LocalPath/1_161/hA2018_Final_NoRadCorr/$xB/12C_1_161_${GENIEExtension}_Plots_FSI_em.root
+scp $JLabAccount:$JLabPath/genie_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root $LocalPath/1_161/${GENIEExtension}/$xB/12C_1_161_${GENIEExtension}_Plots_FSI_em.root
 scp $JLabAccount:$JLabPath/genie_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root $LocalPath/2_261/${GENIEExtension}/$xB/4He_2_261_${GENIEExtension}_Plots_FSI_em.root
 scp $JLabAccount:$JLabPath/genie_e2a_ep_C12_2261_neutrino6_united4_radphot_test.root $LocalPath/2_261/${GENIEExtension}/$xB/12C_2_261_${GENIEExtension}_Plots_FSI_em.root
 scp $JLabAccount:$JLabPath/genie_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test.root $LocalPath/2_261/${GENIEExtension}/$xB/56Fe_2_261_${GENIEExtension}_Plots_FSI_em.root
@@ -23,7 +23,7 @@ scp $JLabAccount:$JLabPath/genie_e2a_ep_4He_4461_neutrino6_united4_radphot_test.
 scp $JLabAccount:$JLabPath/genie_e2a_ep_C12_4461_neutrino6_united4_radphot_test.root $LocalPath/4_461/${GENIEExtension}/$xB/12C_4_461_${GENIEExtension}_Plots_FSI_em.root
 scp $JLabAccount:$JLabPath/genie_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test.root $LocalPath/4_461/${GENIEExtension}/$xB/56Fe_4_461_${GENIEExtension}_Plots_FSI_em.root
 
-# Data Samples
+##### Data Samples
 
 scp $JLabAccount:$JLabPath/data_e2a_ep_C12_1161_neutrino6_united4_radphot_test.root $LocalPath/1_161/$DataExtension/$xB/12C_1_161_${DataExtension}_Plots_FSI_em.root
 scp $JLabAccount:$JLabPath/data_e2a_ep_4He_2261_neutrino6_united4_radphot_test.root $LocalPath/2_261/$DataExtension/$xB/4He_2_261_${DataExtension}_Plots_FSI_em.root

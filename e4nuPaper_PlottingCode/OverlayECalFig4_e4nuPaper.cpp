7528647ffe3e385e278f8d272b74a2153dc996f4
afropapp13 <afropapp@yahoo.gr>
2020-03-02 07:54:44 -0600

    minor changes in fig 3,4 of main paper

--- e4nuPaper_PlottingCode/OverlayECalFig4_e4nuPaper.cpp
+++ e4nuPaper_PlottingCode/OverlayECalFig4_e4nuPaper.cpp
@@ -66,7 +66,6 @@ void OverlayECalFig4_e4nuPaper() {
 
 	SetOffsetAndSize();
 	TGaxis::SetMaxDigits(5);
-//	TGaxis::SetExponentOffset(-0.1, 1., "y");
 
 	int Ndivisions = 5;
 	int LineWidth = 1;
@@ -86,10 +85,8 @@ void OverlayECalFig4_e4nuPaper() {
 	// ------------------------------------------------------------------------
 
 	// Larry/Axel's suggestion for scaling the last 2 bins by EnhaceTail
-//	double EnhaceTail = 1./4.;
-//	double EnhaceTail = 1./2.;
+
 	double EnhaceTail = 1./3.;
-//	double EnhaceTail = 1.;
 
 	std::vector<TString> xBCut; std::vector<TString> nucleus; std::vector<TString> JustNucleus; std::vector<TString> LabelsOfSamples; 
 	std::vector<TString> E; std::vector<double> DoubleE;
@@ -145,7 +142,7 @@ void OverlayECalFig4_e4nuPaper() {
 
 	for (int WhichxBCut = 0; WhichxBCut < NxBCuts; WhichxBCut ++) {
 
-		TCanvas* PlotCanvas = new TCanvas(xBCut[WhichxBCut],xBCut[WhichxBCut],205,34,2000,768);
+		TCanvas* PlotCanvas = new TCanvas(xBCut[WhichxBCut],xBCut[WhichxBCut],205,34,1600,900);
 //			205,34,1024,768);
 //			205,34,768,768);
 
@@ -157,19 +154,24 @@ void OverlayECalFig4_e4nuPaper() {
 
 			for (int WhichEnergy = 0; WhichEnergy < NEnergies; WhichEnergy ++) {
 
+double MaxHeight = 2.8; // In order to use y-axis ticks with common scale, constraint range between (0,MaxHeight)
+
 				// Loop over the nuclei
 
 				for (int WhichNucleus = 0; WhichNucleus < NNuclei; WhichNucleus ++) {
+
+if (nucleus[WhichNucleus] == "56Fe") { MaxHeight = MaxHeight * 0.88; }
 
 					// ---------------------------------------------------------------------------------------------------------------------------
 
 					// Dimensions of TPads
 
-					double Xmin = 0.07, Xmax = 1.;
-					double Ymax = 1., Ymin = 0.1;
+					double Xmin = 0.14, Xmax = 1.;
+					double Ymax = 1., Ymin = 0.05;
 					double Xstep = (Xmax - Xmin) / 3.;
 					double Ystep = ( Ymax - Ymin  ) / 2.;
 					double XMinPad = Xmin + WhichEnergy * Xstep, XMaxPad = Xmin + ( WhichEnergy + 1) * Xstep;
+if (DoubleE[WhichEnergy] == 1.161 ) { XMinPad = XMinPad - 0.05; }
 					double YMinPad = Ymax - ( WhichNucleus + 1) * Ystep, YMaxPad = Ymax - WhichNucleus * Ystep;
 					double space = 0.07;
 
@@ -184,10 +186,13 @@ void OverlayECalFig4_e4nuPaper() {
 					pad->cd();
 
 					pad->SetBottomMargin(0.15);
-					pad->SetTopMargin(0.);
+					pad->SetTopMargin(0.0);
+					if (nucleus[WhichNucleus] == "12C") { pad->SetTopMargin(0.01); }
 					pad->SetLeftMargin(0.);
-					pad->SetRightMargin(0.);
-					pad->SetFrameBorderSize(3);
+if (DoubleE[WhichEnergy] == 1.161 ) { pad->SetLeftMargin(0.05); }
+					pad->SetRightMargin(0.0);
+					if (DoubleE[WhichEnergy] == 4.461 ) { pad->SetRightMargin(0.01); }
+					pad->SetFrameBorderSize(10);
 
 					// ---------------------------------------------------------------------------------------------------------------------------
 
@@ -199,11 +204,11 @@ void OverlayECalFig4_e4nuPaper() {
 
 					Plots.clear();
 
-					TLegend* legGenieBlackLine = new TLegend(0.1,0.5,0.6,1.);
+					TLegend* legGenieBlackLine = new TLegend(0.1,0.5,0.54,1.);
 					legGenieBlackLine->SetNColumns(1);
 					legGenieBlackLine->SetTextFont(FontStyle); 
 
-					TLegend* legGenieBreak = new TLegend(0.1,0.,0.9,0.5);
+					TLegend* legGenieBreak = new TLegend(0.1,0.,1.,0.5);
 					legGenieBreak->SetNColumns(2);
 					legGenieBreak->SetTextFont(FontStyle);
 
@@ -230,7 +235,7 @@ void OverlayECalFig4_e4nuPaper() {
 
 						Plots[WhichFSIModel]->GetXaxis()->SetLabelFont(FontStyle);
 						Plots[WhichFSIModel]->GetXaxis()->SetTitleFont(FontStyle);
-						Plots[WhichFSIModel]->GetXaxis()->SetLabelSize(2*TextSize);
+						Plots[WhichFSIModel]->GetXaxis()->SetLabelSize(1.2*TextSize);
 						Plots[WhichFSIModel]->GetXaxis()->SetTitleSize(0.);
 						Plots[WhichFSIModel]->GetXaxis()->SetNdivisions(Ndivisions);
 
@@ -241,8 +246,12 @@ void OverlayECalFig4_e4nuPaper() {
 						Plots[WhichFSIModel]->GetYaxis()->SetLabelFont(FontStyle);
 						Plots[WhichFSIModel]->GetYaxis()->SetTitleFont(FontStyle);
 						Plots[WhichFSIModel]->GetYaxis()->SetLabelSize(0.);
+						Plots[WhichFSIModel]->GetYaxis()->SetLabelOffset(0.013);
+//if (DoubleE[WhichEnergy] == 1.161) { Plots[WhichFSIModel]->GetYaxis()->SetLabelSize(0.07); }	
+Plots[WhichFSIModel]->GetYaxis()->SetLabelSize(1.2*TextSize);
+
+
 						Plots[WhichFSIModel]->GetYaxis()->SetTitle("");
-						Plots[WhichFSIModel]->GetYaxis()->SetNdivisions(0.);
 
 						// --------------------------------------------------------------------------------------
 
@@ -280,6 +289,9 @@ void OverlayECalFig4_e4nuPaper() {
 						// Accounting for the fact that the bin width might not be constant
 
 						ReweightPlots(Plots[WhichFSIModel]);
+
+if (DoubleE[WhichEnergy] == 1.161) { Plots[WhichFSIModel]->Scale(1./4.); }
+Plots[WhichFSIModel]->GetYaxis()->SetNdivisions(3);
 
 						// --------------------------------------------------------------------------------------
 
@@ -333,6 +345,8 @@ void OverlayECalFig4_e4nuPaper() {
 								BreakDownPlots[j-1]->SetLineStyle(Style[j-1]);
 								BreakDownPlots[j-1]->Scale(ScalingFactor);
 
+if (DoubleE[WhichEnergy] == 1.161) { BreakDownPlots[j-1]->Scale(1./4.); }
+
 								//-----------------------------------------------------------------------------------------------
 
 								//Larry's suggestion because ECal has a sharp peak and a low tail 
@@ -373,12 +387,15 @@ void OverlayECalFig4_e4nuPaper() {
 
 						// Max, min, title & # divisions
 
-						double localmax = Plots[WhichFSIModel]->GetMaximum();
-						if (localmax > max) { max = localmax; }
-						double height = 1.15;
-						if ( xBCut[WhichxBCut] == "xBCut" ) { height = 1.1; }
-						if ( DoubleE[WhichEnergy] == 2.261 ) { height = 1.4; }
-						Plots[0]->GetYaxis()->SetRangeUser(0.,height*max);
+//						double localmax = Plots[WhichFSIModel]->GetMaximum();
+//						if (localmax > max) { max = localmax; }
+//						double height = 1.15;
+//						if ( xBCut[WhichxBCut] == "xBCut" ) { height = 1.1; }
+//						if ( DoubleE[WhichEnergy] == 2.261 ) { height = 1.4; }
+//						Plots[0]->GetYaxis()->SetRangeUser(0.,height*max);
+
+
+Plots[0]->GetYaxis()->SetRangeUser(0.,MaxHeight);
 
 						double localmin = Plots[WhichFSIModel]->GetBinContent(Plots[WhichFSIModel]->FindBin(4)); // multiplicity 4 is the highest one in data
 						if (localmin < min && localmin != 0) { min = localmin; }
@@ -388,7 +405,8 @@ void OverlayECalFig4_e4nuPaper() {
 
 						// -------------------------------------------------------------------------------------------------
 
-						TLine* line = new TLine(0.95*DoubleE[WhichEnergy],0.,0.95*DoubleE[WhichEnergy],height*max);
+//						TLine* line = new TLine(0.95*DoubleE[WhichEnergy],0.,0.95*DoubleE[WhichEnergy],height*max);
+TLine* line = new TLine(0.95*DoubleE[WhichEnergy],0.,0.95*DoubleE[WhichEnergy],MaxHeight);
 						line->SetLineColor(kBlack); 
 						line->SetLineWidth(LineWidth);
 						if ( FSILabel[WhichFSIModel] == "Genie" && DoubleE[WhichEnergy] == 2.261) { line->Draw(); }
@@ -406,28 +424,12 @@ void OverlayECalFig4_e4nuPaper() {
 
 						} else { 
 
-//							Plots[WhichFSIModel]->Draw("hist same"); // draw them as histos
-							Plots[WhichFSIModel]->Draw("C hist same");  // draw them as lines
+							Plots[WhichFSIModel]->Draw("C hist same");  // "C hist same" draw them as lines // "hist same" draw them as histos
 							Plots[0]->Draw("e same"); 
 
 						}
 
 					} // End of the loop over the FSI Models 
-
-					if ( nucleus[WhichNucleus] == "12C" && DoubleE[WhichEnergy] == 1.161) { 
-
-//						legGenieBlackLine->SetBorderSize(0); 
-						legGenieBlackLine->SetNColumns(1); 
-//						legGenieBlackLine->SetTextSize(1.5*TextSize); 
-//						legGenieBlackLine->SetTextFont(FontStyle); 
-						//legGenieBlackLine->Draw(); 
-
-//						legGenieBreak->SetBorderSize(0); 
-//						legGenieBreak->SetTextSize(1.5*TextSize);
-//						legGenieBreak->SetTextFont(FontStyle);  
-						//legGenieBreak->Draw();
-
-					}
 
 					// -----------------------------------------------------------------------------------------------------------------------------------------
 
@@ -441,7 +443,7 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latexCarbon;
 		latexCarbon.SetTextFont(FontStyle);
-		latexCarbon.SetTextSize(TextSize);
+		latexCarbon.SetTextSize(0.8*TextSize);
 		PlotCanvas->cd();
 		latexCarbon.DrawLatexNDC(0.005,0.77,"^{12}C");
 
@@ -449,13 +451,13 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latexIron;
 		latexIron.SetTextFont(FontStyle);
-		latexIron.SetTextSize(TextSize);
+		latexIron.SetTextSize(0.8*TextSize);
 		PlotCanvas->cd();
-		latexIron.DrawLatexNDC(0.325,0.37,"^{56}Fe");
+		latexIron.DrawLatexNDC(0.3,0.385,"^{56}Fe");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 
-		TPad* pad1GeV = new TPad("pad1GeV","pad1GeV",0.09,0.89,0.26,0.99,21); 
+		TPad* pad1GeV = new TPad("pad1GeV","pad1GeV",0.125,0.89,0.315,0.99,21); 
 		pad1GeV->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		pad1GeV->Draw();
@@ -463,12 +465,12 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latex1GeV;
 		latex1GeV.SetTextFont(FontStyle);
-		latex1GeV.SetTextSize(8*TextSize);
-		latex1GeV.DrawLatexNDC(0.03,0.4,"1.161 GeV");
+		latex1GeV.SetTextSize(5*TextSize);
+		latex1GeV.DrawLatexNDC(0.04,0.45,"1.161 GeV");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 
-		TPad* pad2GeV = new TPad("pad2GeV","pad2GeV",0.4,0.89,0.6,0.99,21); 
+		TPad* pad2GeV = new TPad("pad2GeV","pad2GeV",0.45,0.89,0.6,0.99,21); 
 		pad2GeV->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		pad2GeV->Draw();
@@ -476,12 +478,12 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latex2GeV;
 		latex2GeV.SetTextFont(FontStyle);
-		latex2GeV.SetTextSize(8*TextSize);
-		latex2GeV.DrawLatexNDC(0.05,0.4,"2.261 GeV");
+		latex2GeV.SetTextSize(5*TextSize);
+		latex2GeV.DrawLatexNDC(0.06,0.45,"2.261 GeV");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 
-		TPad* pad4GeV = new TPad("pad4GeV","pad4GeV",0.7,0.89,0.9,0.99,21); 
+		TPad* pad4GeV = new TPad("pad4GeV","pad4GeV",0.73,0.89,0.9,0.99,21); 
 		pad4GeV->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		pad4GeV->Draw();
@@ -489,13 +491,12 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latex4GeV;
 		latex4GeV.SetTextFont(FontStyle);
-		latex4GeV.SetTextSize(8*TextSize);
-		latex4GeV.DrawLatexNDC(0.1,0.4,"4.461 GeV");
+		latex4GeV.SetTextSize(5*TextSize);
+		latex4GeV.DrawLatexNDC(0.11,0.45,"4.461 GeV");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 
-//		TPad* padPmiss = new TPad("padPmiss","padPmiss",0.55,0.03,0.9,0.16,21); 
-		TPad* padPmiss = new TPad("padPmiss","padPmiss",0.4,0.03,0.75,0.16,21); 
+		TPad* padPmiss = new TPad("padPmiss","padPmiss",0.42,0.01,0.78,0.14,21); 
 		padPmiss->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		padPmiss->Draw();
@@ -503,12 +504,25 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latexPmiss;
 		latexPmiss.SetTextFont(FontStyle);
-		latexPmiss.SetTextSize(8*TextSize);
-		latexPmiss.DrawLatexNDC(0.05,0.5,"(e,e'p)_{1p0#pi} E_{cal} [GeV]");
+		latexPmiss.SetTextSize(5*TextSize);
+		latexPmiss.DrawLatexNDC(0.1,0.5,"(e,e'p)_{1p0#pi} E_{cal} [GeV]");
 
 		// -------------------------------------------------------------------------------------------------
 	
-		TPad* padx13C12 = new TPad("padx13C12","padx13C12",0.65,0.89,0.688,0.99,21); 
+		TPad* padx14C12 = new TPad("padx14C12","padx14C12",0.155,0.65,0.205,0.72,21); 
+		padx14C12->SetFillColor(kWhite); 
+		PlotCanvas->cd();
+		padx14C12->Draw();
+		padx14C12->cd();
+
+		TLatex latexx14C12;
+		latexx14C12.SetTextFont(FontStyle);
+		latexx14C12.SetTextSize(8*TextSize);
+		latexx14C12.DrawLatexNDC(0.,0.4,"x1/4");
+
+		// -------------------------------------------------------------------------------------------------
+	
+		TPad* padx13C12 = new TPad("padx13C12","padx13C12",0.675,0.91,0.71,0.98,21); 
 		padx13C12->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		padx13C12->Draw();
@@ -516,12 +530,12 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latexx13C12;
 		latexx13C12.SetTextFont(FontStyle);
-		latexx13C12.SetTextSize(8*TextSize);
+		latexx13C12.SetTextSize(7.5*TextSize);
 		latexx13C12.DrawLatexNDC(0.,0.4,"x1/3");
 
 		// -------------------------------------------------------------------------------------------------
 	
-		TPad* padx13Fe56 = new TPad("padx13Fe56","padx13Fe56",0.65,0.51,0.688,0.61,21); 
+		TPad* padx13Fe56 = new TPad("padx13Fe56","padx13Fe56",0.675,0.51,0.71,0.58,21); 
 		padx13Fe56->SetFillColor(kWhite); 
 		PlotCanvas->cd();
 		padx13Fe56->Draw();
@@ -529,7 +543,7 @@ void OverlayECalFig4_e4nuPaper() {
 
 		TLatex latexx13F56;
 		latexx13F56.SetTextFont(FontStyle);
-		latexx13F56.SetTextSize(8*TextSize);
+		latexx13F56.SetTextSize(7.5*TextSize);
 		latexx13F56.DrawLatexNDC(0.,0.42,"x1/3");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
@@ -537,16 +551,16 @@ void OverlayECalFig4_e4nuPaper() {
 		// Extra pad for the legend
 
 		PlotCanvas->cd();
-		TPad* padLegend = new TPad("padLegend","padLegend",0.07,0.21,0.32,0.55, 21); 
+		TPad* padLegend = new TPad("padLegend","padLegend",0.07,0.19,0.3,0.53, 21); 
 		padLegend->SetFillColor(kWhite); 
 		padLegend->Draw();
 		padLegend->cd();
 
-		legGenieBlackLine->SetTextSize(2.5*TextSize); 
+		legGenieBlackLine->SetTextSize(2.*TextSize); 
 		legGenieBlackLine->SetBorderSize(0); 
 		legGenieBlackLine->Draw();
 
-		legGenieBreak->SetTextSize(2.5*TextSize); 
+		legGenieBreak->SetTextSize(2.*TextSize); 
 		legGenieBreak->SetBorderSize(0); 
 		legGenieBreak->Draw();
 
@@ -555,34 +569,82 @@ void OverlayECalFig4_e4nuPaper() {
 		// Extra pad for the Y-axis units carbon
 
 		PlotCanvas->cd();
-		TPad* padTitle = new TPad("padTitle","padTitle",0.047,0.57,0.069,1., 21); 
+		TPad* padTitle = new TPad("padTitle","padTitle",0.065,0.58,0.09,1., 21); 
 		padTitle->SetFillColor(kWhite); 
 		padTitle->Draw();
 		padTitle->cd();
 
 		TLatex latexYTitle;
 		latexYTitle.SetTextFont(FontStyle);
-		latexYTitle.SetTextSize(12*TextSize);
+		latexYTitle.SetTextSize(11*TextSize);
 		latexYTitle.SetTextColor(kBlack);
 		latexYTitle.SetTextAngle(90);
-		latexYTitle.DrawLatexNDC(0.7,0.1,"Weighted Counts");
+		latexYTitle.DrawLatexNDC(0.8,0.03,"Weighted Events / GeV");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 
-//		// Extra pad for the Y-axis units iron
+		// Extra pad for the Y-axis units iron
 
-//		PlotCanvas->cd();
-//		TPad* padTitleFe = new TPad("padTitleFe","padTitleFe",0.2,0.1,0.4,0.5,21); 
-//		padTitleFe->SetFillColor(kBlack); 
-//		padTitleFe->Draw();
-//		padTitleFe->cd();
+		PlotCanvas->cd();
+		TPad* padTitleFe = new TPad("padTitleFe","padTitleFe",0.37,0.18,0.4,0.55,21); 
+		padTitleFe->SetFillColor(kWhite); 
+		padTitleFe->Draw();
+		padTitleFe->cd();
 
-//		TLatex latexYTitleFe;
-//		latexYTitleFe.SetTextFont(FontStyle);
-//		latexYTitleFe.SetTextSize(12*TextSize);
-//		latexYTitleFe.SetTextColor(kBlack);
-//		latexYTitleFe.SetTextAngle(90);
-//		latexYTitleFe.DrawLatexNDC(0.6,0.1,"Weighted Counts");
+		TLatex latexYTitleFe;
+		latexYTitleFe.SetTextFont(FontStyle);
+		latexYTitleFe.SetTextSize(9*TextSize);
+		latexYTitleFe.SetTextColor(kBlack);
+		latexYTitleFe.SetTextAngle(90);
+		latexYTitleFe.DrawLatexNDC(0.8,0.03,"Weighted Events / GeV");
+
+		// -----------------------------------------------------------------------------------------------------------------------------------------
+
+		// Extra pad for the Y-axis 0. point
+
+		PlotCanvas->cd();
+		TPad* padTitleZero = new TPad("padTitleZero","padTitleZero",0.41,0.17,0.426,0.22,21); 
+		padTitleZero->SetFillColor(kWhite); 
+		padTitleZero->Draw();
+		padTitleZero->cd();
+
+		TLatex latexYTitleZero;
+		latexYTitleZero.SetTextFont(FontStyle);
+		latexYTitleZero.SetTextSize(20*TextSize);
+		latexYTitleZero.SetTextColor(kBlack);
+		latexYTitleZero.DrawLatexNDC(0.,0.1,"0");
+
+		// -----------------------------------------------------------------------------------------------------------------------------------------
+
+		// Extra pad for the Y-axis 1. point
+
+		PlotCanvas->cd();
+		TPad* padTitleOne = new TPad("padTitleOne","padTitleOne",0.41,0.34,0.426,0.39,21); 
+		padTitleOne->SetFillColor(kWhite); 
+		padTitleOne->Draw();
+		padTitleOne->cd();
+
+		TLatex latexYTitleOne;
+		latexYTitleOne.SetTextFont(FontStyle);
+		latexYTitleOne.SetTextSize(20*TextSize);
+		latexYTitleOne.SetTextColor(kBlack);
+		latexYTitleOne.DrawLatexNDC(0.,0.1,"1");
+
+		// -----------------------------------------------------------------------------------------------------------------------------------------
+
+		// Extra pad for the Y-axis 2. point
+
+		PlotCanvas->cd();
+		TPad* padTitleTwo = new TPad("padTitleTwo","padTitleTwo",0.41,0.5,0.426,0.55,21); 
+		padTitleTwo->SetFillColor(kWhite); 
+		padTitleTwo->Draw();
+		padTitleTwo->cd();
+
+		TLatex latexYTitleTwo;
+		latexYTitleTwo.SetTextFont(FontStyle);
+		latexYTitleTwo.SetTextSize(20*TextSize);
+		latexYTitleTwo.SetTextColor(kBlack);
+		latexYTitleTwo.DrawLatexNDC(0.,0.1,"2");
 
 		// -----------------------------------------------------------------------------------------------------------------------------------------
 

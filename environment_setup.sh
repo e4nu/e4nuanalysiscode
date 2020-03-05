0f6b443659665e56258ad42339218d3b46592de8
afropapp13 <afropapp@yahoo.gr>
2020-03-05 17:56:25 -0600

    adding the setup script

---
+++ environment_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                                                                     
+
+source /grid/fermiapp/products/uboone/setup_uboone.sh
+
+setup uboonecode v06_26_01_08 -q e10:prof

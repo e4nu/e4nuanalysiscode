11f27d0ae5047368366bf8993643d4c89f32d4bd
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-09-18 04:54:22 -0500

    adding example

---
+++ Documentation/run_1p1pi_plotting.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+cd $E4NUANALYSIS
+
+# 1GeV 1p1pim mc files
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+# 1GeV 1p1pip mc files
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad_true.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad_truereco.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+# 2GeV 1p1pim mc files
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+# 2GeV 1p1pip mc files
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+#
+# 4GeV 1p1pim mc files
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+# 4GeV 1p1pip mc files
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad_true.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+scp -o 'GSSAPIAuthentication=yes' jtenavid@geniegpvm02.fnal.gov:/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pip/e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad_truereco.root  /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/.
+
+# data files
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pimanalysis_e_on_1000060120_1161MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pimanalysis_e_on_1000060120_2261MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pimanalysis_e_on_1000060120_4461MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pipanalysis_e_on_1000060120_1161MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pipanalysis_e_on_1000060120_2261MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1p1pipanalysis_e_on_1000060120_4461MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pimanalysis_e_on_1000060120_1161MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pimanalysis_e_on_1000060120_2261MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pimanalysis_e_on_1000060120_4461MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pipanalysis_e_on_1000060120_1161MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pipanalysis_e_on_1000060120_2261MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+scp ifarm:/u/home/jtena/Software/e4nuanalysiscode/e4nuanalysis_1pipanalysis_e_on_1000060120_4461MeV_clas6data.root /Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+
+MCLOC=/Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/mc_files/
+DATALOC=/Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi/data_files/
+OUTLOC=/Users/juliatenavidal/Desktop/Postdoc/e4nu/PionAnalysis/1p1pi
+OBS=HadAlphaT,HadDeltaPT,HadDeltaPT,HadDeltaPTy,HadDeltaPhiT,InferedNucleonMom,MissingMomentum,MissingAngle
+# plot 1GeV analysis, 1p1pim
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC --output_name clas6analysis_1p1pim_1GeV --input_mc_files e4nuanalysis_1p1pimanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad,e4nuanalysis_1p1pimanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{-} 1GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pimanalysis_e_on_1000060120_1161MeV_clas6data --data_name "CLAS6 1p1#pi^{-} 1GeV"
+
+# plot 2GeV analysis, 1p1pim
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC --output_name clas6analysis_1p1pim_2GeV --input_mc_files e4nuanalysis_1p1pimanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad,e4nuanalysis_1p1pimanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{-} 2GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pimanalysis_e_on_1000060120_2261MeV_clas6data --data_name "CLAS6 1p1#pi^{-} 2GeV"
+
+# plot 4GeV analysis, 1p1pim
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC--output_name clas6analysis_1p1pim_3GeV --input_mc_files e4nuanalysis_1p1pimanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad,e4nuanalysis_1p1pimanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{-} 4GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pimanalysis_e_on_1000060120_4461MeV_clas6data --data_name "CLAS6 1p1#pi^{-} 4GeV"
+
+# plot 1GeV analysis, 1p1pip
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC --output_name clas6analysis_1p1pip_1GeV --input_mc_files e4nuanalysis_1p1pipanalysis_G18_10a_Q2_01_e_on_1000060120_1161MeV_NoRad,e4nuanalysis_1p1pipanalysis_G18_10b_Q2_01_e_on_1000060120_1161MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_01_e_on_1000060120_1161MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{+} 1GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pipanalysis_e_on_1000060120_1161MeV_clas6data --data_name "CLAS6 1p1#pi^{+} 1GeV"
+
+# plot 2GeV analysis, 1p1pip
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC --output_name clas6analysis_1p1pip_2GeV --input_mc_files e4nuanalysis_1p1pipanalysis_G18_10a_Q2_04_e_on_1000060120_2261MeV_NoRad,e4nuanalysis_1p1pipanalysis_G18_10b_Q2_04_e_on_1000060120_2261MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_04_e_on_1000060120_2261MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{+} 2GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pipanalysis_e_on_1000060120_2261MeV_clas6data --data_name "CLAS6 1p1#pi^{+} 2GeV"
+
+# plot 4GeV analysis, 1p1pip
+./plote4nuanalysis --mc_location $MCLOC --output_location $OUTLOC --output_name clas6analysis_1p1pip_4GeV --input_mc_files e4nuanalysis_1p1pipanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad,e4nuanalysis_1p1pipanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad --model_names G18_10a,G18_10b --nofsi_file e4nuanalysis_1p1pipanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad --title "CLAS6 C^{12} 1p1#pi^{+} 4GeV" --observable_list $OBS --data_location $DATALOC --input_data_file e4nuanalysis_1p1pipanalysis_e_on_1000060120_4461MeV_clas6data --data_name "CLAS6 1p1#pi^{+} 4GeV"

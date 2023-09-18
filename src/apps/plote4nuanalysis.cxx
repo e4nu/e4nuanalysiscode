1d92d2927507eba6d780d0bb7735b671d0a73fa7
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-09-18 04:59:06 -0500

    adding fsi

--- src/apps/plote4nuanalysis.cxx
+++ src/apps/plote4nuanalysis.cxx
@@ -135,9 +135,9 @@ int main( int argc, char* argv[] ) {
 
   // Loop over observables
   for( unsigned int i = 0 ; i < observables.size(); ++i ){
-    string acceptance_file = plotting::ComputeAcceptance( mc_files, observables[i], title, mc_location, output_location, output_name ) ;
     vector<string> root_files = mc_files;
-    if( nofsi_file != "" && i == 0 ) { root_files.push_back(nofsi_file); model_names.push_back("No FSI");}
+    string acceptance_file = plotting::ComputeAcceptance( root_files, observables[i], title, mc_location, output_location, output_name ) ;
+    if( nofsi_file != "" ) { root_files.push_back(nofsi_file); model_names.push_back("No FSI");}
     plotting::Plot1DXSec( root_files, data_file, acceptance_file, observables[i], title, data_name, model_names, mc_location, data_location, output_location, output_name, plot_data ) ;
   }
 

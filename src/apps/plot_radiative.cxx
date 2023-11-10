a50b4525a8661388fe1d9d7739cd9c63dfa57fb4
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-11-10 12:08:11 +0100

    adding changes

--- src/apps/plot_radiative.cxx
+++ src/apps/plot_radiative.cxx
@@ -6,11 +6,14 @@
 #include "utils/RadiativeCorrUtils.h"
 #include "conf/ConstantsI.h"
 #include "conf/ParticleI.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TAxis.h"
 
 using namespace std;
 using namespace e4nu;
 using namespace e4nu::utils;
-using namespace e4nu::conf; 
+using namespace e4nu::conf;
 
 /////////////////////////////////////////////////////////////////
 // Options:                                                    //
@@ -20,22 +23,52 @@ int main( int argc, char* argv[] ) {
 
   std::cout << "Plotting e4nu analysis radiative corrections..." << std::endl;
 
-  double EBeam = 1; // To be configurable 
+  vector<double> EBeam = {1,2,4};
   double W = kNucleonMass;
-  double maxQ2 = 2*kNucleonMass*(EBeam-kElectronMass);
   double minQ2 = 0.01;
-  double nbins = 100; 
-  double binwidth= (maxQ2-minQ2)/nbins;
-  vector<double> Q2bins, QELVertex, QELVaumm ; 
-  for( unsigned int i = 0 ; i < nbins ; ++i ) { 
-    double Q2 = minQ2 + i*binwidth ;
-    Q2bins.push_back(Q2);
-    QELVertex.push_back(utils::RadCorrQELVertex(Q2));
-    QELVaumm.push_back(utils::RadCorrQELVacumm(Q2));
+  double nbins = 100;
+  vector<double> maxQ2,binwidth ;
+  for(unsigned int i = 0 ; i < EBeam.size(); ++i ) {
+      maxQ2.push_back(2*kNucleonMass*(EBeam[i]-kElectronMass));
+      binwidth.push_back((maxQ2[i]-minQ2)/nbins);
   }
 
-  TGraph * gRadCorrQELVertex = new TGraph( nbins, &Q2bins[0], &QELVertex[0] );
-  TGraph * gRadCorrQELVertex = new TGraph( nbins, &Q2bins[&QEL], x[0] );
+  vector<vector<double>> Q2bins, QELVertex, QELVaumm ;
+  for( unsigned int i = 0 ; i < EBeam.size() ; ++i ) {
+    vector<double> Q2v, QELVertexv, QELVaummv;
+    for( unsigned int j = 0 ; j < nbins ; ++j ) {
+      double Q2 = minQ2 + j*binwidth[i] ;
+      Q2v.push_back(Q2);
+      QELVertexv.push_back(utils::RadCorrQELVertex(Q2));
+      QELVaummv.push_back(utils::RadCorrQELVacumm(Q2));
+    }
+    Q2bins.push_back(Q2v);
+    QELVaumm.push_back(QELVaummv);
+    QELVertex.push_back(QELVertexv);
+  }
 
-  return 0 ;
+  TCanvas * c1 = new TCanvas("c1","c1",200,10,700,500);
+  TGraph* gRadCorrQELVertex=new TGraph( nbins, &Q2bins[0][0], &QELVertex[0][0]);
+  TGraph* gRadCorrQELVacumm=new TGraph( nbins, &Q2bins[0][0], &QELVaumm[0][0]);
+
+  gRadCorrQELVertex->SetLineWidth(2);
+  gRadCorrQELVertex->SetLineColor(216);
+  gRadCorrQELVertex->GetXaxis()->SetTitle("Q^{2}[GeV^{2}]");
+  gRadCorrQELVertex->GetYaxis()->SetTitle("#delta_{vertex}");
+  gRadCorrQELVertex->GetXaxis()->CenterTitle();
+  gRadCorrQELVertex->GetYaxis()->CenterTitle();
+  gRadCorrQELVertex->Draw("AC");
+
+  c1->SaveAs("RadCorrectionVertex.pdf");
+
+  gRadCorrQELVacumm->SetLineWidth(2);
+  gRadCorrQELVacumm->SetLineColor(209);
+  gRadCorrQELVacumm->GetXaxis()->SetTitle("Q^{2}[GeV^{2}]");
+  gRadCorrQELVacumm->GetYaxis()->SetTitle("#delta_{vac}");
+  gRadCorrQELVacumm->GetXaxis()->CenterTitle();
+  gRadCorrQELVacumm->GetYaxis()->CenterTitle();
+  gRadCorrQELVacumm->Draw("AC");
+
+  c1->SaveAs("RadCorrectionVacumm.pdf");
+ return 0 ;
 }

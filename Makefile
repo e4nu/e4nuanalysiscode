e87f99553607a56619fd5dcd317bf163c6f0af5f
jtenavidal <jtenavidal@tauex.tau.ac.il>
2023-11-20 04:45:21 -0600

    adding new plotting app

--- Makefile
+++ Makefile
@@ -33,13 +33,13 @@ PHYSICS_OBJS := $(patsubst $(SRCDIR)/%.cxx,$(OBJDIR)/%.o,$(PHYSICS_SRCS))
 ANALYSIS_OBJS := $(patsubst $(SRCDIR)/%.cxx,$(OBJDIR)/%.o,$(ANALYSIS_SRCS))
 APP_OBJS := $(patsubst $(SRCDIR)/%.cxx,$(OBJDIR)/%.o,$(APP_SRCS)) 
 
-all: e4nuanalysis plote4nuanalysis plot_radiative radiate_flux
+all: e4nuanalysis plot_e4nuanalysis plot_radiative radiate_flux plot_bkg
  
 e4nuanalysis: $(SRCDIR)/apps/e4nuanalysis.cxx $(UTILS_OBJS) $(PLOTTING_OBJS) $(CONF_OBJS) $(PHYSICS_OBJS) $(ANALYSIS_OBJS) $(APP_OBJS)
 	@mkdir -p $(@D)
 	$(CXX) $(CXXFLAGS) $(ROOTLIBS) $(OBJDIR)/utils/*.o  $(OBJDIR)/plotting/*.o $(OBJDIR)/physics/*.o $(OBJDIR)/conf/*.o $(OBJDIR)/analysis/*.o $< -o $@
 
-plote4nuanalysis: $(SRCDIR)/apps/plote4nuanalysis.cxx $(UTILS_OBJS) $(PLOTTING_OBJS) $(CONF_OBJS) $(PHYSICS_OBJS) $(ANALYSIS_OBJS) $(APP_OBJS)
+plot_e4nuanalysis: $(SRCDIR)/apps/plote4nuanalysis.cxx $(UTILS_OBJS) $(PLOTTING_OBJS) $(CONF_OBJS) $(PHYSICS_OBJS) $(ANALYSIS_OBJS) $(APP_OBJS)
 	@mkdir -p $(@D)
 	$(CXX) $(CXXFLAGS) $(ROOTLIBS) $(OBJDIR)/utils/*.o  $(OBJDIR)/plotting/*.o $(OBJDIR)/physics/*.o $(OBJDIR)/conf/*.o $(OBJDIR)/analysis/*.o $< -o $@
 
@@ -51,14 +51,19 @@ radiate_flux: $(SRCDIR)/apps/radiate_flux.cxx $(UTILS_OBJS) $(PLOTTING_OBJS) $(C
 	@mkdir -p $(@D)
 	$(CXX) $(CXXFLAGS) $(ROOTLIBS) $(OBJDIR)/utils/*.o  $(OBJDIR)/plotting/*.o $(OBJDIR)/physics/*.o $(OBJDIR)/conf/*.o $(OBJDIR)/analysis/*.o $< -o $@
 
+plot_bkg: $(SRCDIR)/apps/plot_BkgCheck.cxx $(UTILS_OBJS) $(PLOTTING_OBJS) $(CONF_OBJS) $(PHYSICS_OBJS) $(ANALYSIS_OBJS) $(APP_OBJS)
+	@mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS) $(ROOTLIBS) $(OBJDIR)/utils/*.o  $(OBJDIR)/plotting/*.o $(OBJDIR)/physics/*.o $(OBJDIR)/conf/*.o $(OBJDIR)/analysis/*.o $< -o $@
+
 $(OBJDIR)/%.o: $(SRCDIR)/%.cxx
 	@mkdir -p $(@D)	
 	$(CXX) $(CXXFLAGS) $(ROOTLIBS) -c $< -o $@
 
 clean:
 	rm -rf $(OBJDIR)/* e4nuanalysis
-	rm -rf $(OBJDIR)/* plote4nuanalysis
+	rm -rf $(OBJDIR)/* plot_e4nuanalysis
 	rm -rf $(OBJDIR)/* plot_radiative
+	rm -rf $(OBJDIR)/* plot_bkg
 
 .PHONY: test
 
---
+++ src/apps/plot_BkgCheck.cxx
@@ -0,0 +1,154 @@
+// __________________________________________________________________________
+/* This app is used to plot the estimated vs true background obtained from  */
+/* The output root file computed with bkg debug mode ("DebugBkg true")      */
+/* An example of how to run the analysis with this mode is available in     */
+/* ConfFiles/example_configuration.txt                                      */
+// __________________________________________________________________________
+#include <iostream>
+#include <vector>
+#include "utils/RadiativeCorrUtils.h"
+#include "plotting/PlottingUtils.h"
+#include "conf/ConstantsI.h"
+#include "conf/ParticleI.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TAxis.h"
+#include "TLegend.h"
+
+using namespace std;
+using namespace e4nu;
+using namespace e4nu::utils;
+using namespace e4nu::conf;
+using namespace e4nu::plotting;
+/////////////////////////////////////////////////////////////////
+// Options:                                                    //
+/////////////////////////////////////////////////////////////////
+
+int main( int argc, char* argv[] ) {
+
+  std::cout << "Plotting e4nu analysis background check..." << std::endl;
+
+  if ( argc == 0 ) {
+    std::cout << "Input file not specified. Abort..." << std::endl;
+    return 0;
+  }
+
+  std::string input_file ;
+  std::string output_file = "bakground_debug.root";
+  std::string observable = "ECal";
+  if( argc > 1 ) { // configure rest of analysis
+    if( ExistArg("input-file",argc,argv)) {
+      input_file = GetArg("input-file",argc,argv);
+      std::cout << " Using " << input_file << " for plotting "<<std::endl;
+    } else { return 0 ;}
+
+    if( ExistArg("output-file",argc,argv)) {
+      output_file = GetArg("output-file",argc,argv);
+    }
+    if( ExistArg("observable",argc,argv)) {
+      observable = GetArg("observable",argc,argv);
+    }
+  }
+
+  TFile* in_root_file = new TFile( input_file.c_str(), "ROOTFile" ) ;
+  if( !in_root_file ) {
+    std::cout << " root file " << input_file << "does not exist" << std::endl;
+    return 0;
+  }
+
+  TCanvas * c  = new TCanvas("","",800,800);
+  c->SetFillColor(0);
+  // Top canvas - distribution
+  // Bottom canvas - relative difference
+  TPad *pad1 = new TPad("pad1","",0,0.4,1,1);
+  TPad *pad2 = new TPad("pad2","",0,0,1,0.4);
+
+  TH1D * h_tot_true = (TH1D*) in_root_file->Get( (observable+"_TotTrueBkg").c_str() ) ;
+  TH1D * h_tot_est = (TH1D*) in_root_file->Get( (observable+"_TotEstBkg").c_str() ) ;
+  TH1D * h_diff_true = (TH1D*) h_tot_est->Clone();
+  h_diff_true->Scale(-1.);
+  h_diff_true->Add(h_tot_true);
+  h_diff_true->Divide(h_tot_true);
+  h_diff_true->Scale(100.); // Relative error in %
+
+  // Setting formatt
+  gStyle->SetFrameBorderMode(0);
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetPadBorderMode(0);
+  gStyle->SetPadColor(0);
+  gStyle->SetCanvasColor(0);
+  gStyle->SetStatColor(0);
+  gStyle->SetFillColor(0);
+  gStyle->SetLegendBorderSize(1);
+  gStyle->SetPaperSize(20,26);
+  gStyle->SetTitleFont(132,"pad");
+  gStyle->SetMarkerStyle(20);
+  gStyle->SetLineStyleString(2,"[12 12]");
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(0);
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+
+  h_tot_true -> SetLineColor(kRed);
+  h_tot_true -> SetMarkerStyle(8);
+  h_tot_true -> SetMarkerColor(kRed);
+  h_tot_true -> SetLineWidth(2);
+  h_tot_true->GetYaxis()->SetLabelSize(0.06);
+  h_tot_true->GetYaxis()->SetTitleSize(0.06);
+  h_tot_true -> SetTitle("Total Background");
+  h_tot_true -> GetXaxis()->SetTitle(plotting::GetAxisLabel(observable,0).c_str());
+  h_tot_true -> GetYaxis()->SetTitle(plotting::GetAxisLabel(observable,1).c_str());
+  h_tot_true -> GetXaxis()->CenterTitle();
+  h_tot_true -> GetYaxis()->CenterTitle();
+  double max = plotting::GetMaximum({h_tot_true,h_tot_est});
+  h_tot_true->GetYaxis()->SetRangeUser(0,max);
+
+  h_tot_est -> SetLineColor(kBlue);
+  h_tot_est -> SetMarkerStyle(8);
+  h_tot_est -> SetMarkerColor(kBlue);
+  h_tot_est -> SetLineWidth(2);
+
+  h_diff_true -> SetLineColor(kBlack);
+  h_diff_true -> SetMarkerStyle(8);
+  h_diff_true -> SetMarkerColor(kBlack);
+  h_diff_true -> SetLineWidth(2);
+
+  h_diff_true -> SetTitle("");
+  h_diff_true -> GetXaxis()->SetTitle(plotting::GetAxisLabel(observable,0).c_str());
+  h_diff_true -> GetYaxis()->SetTitle("Error [%]");
+  h_diff_true -> GetXaxis()->CenterTitle();
+  h_diff_true -> GetYaxis()->CenterTitle();
+  h_diff_true->GetYaxis()->SetNdivisions(6);
+  h_diff_true->GetYaxis()->SetRangeUser(-50,50);
+  h_diff_true->GetXaxis()->SetLabelSize(0.08);
+  h_diff_true->GetYaxis()->SetLabelSize(0.08);
+  h_diff_true->GetXaxis()->SetTitleOffset(1.2);
+  h_diff_true->GetXaxis()->SetTitleSize(0.08);
+  h_diff_true->GetYaxis()->SetTitleSize(0.08);
+
+  auto legend = new TLegend(0.15,0.6,0.35,0.8);
+  legend->AddEntry(h_tot_true, "Total true background");
+  legend->AddEntry(h_tot_est, "Total estimated background");
+  h_tot_true -> GetYaxis()->SetTitle("#Events");
+
+  pad1->Draw();
+  pad1->cd();
+  pad1->SetBottomMargin(0.015);
+  pad1->SetLeftMargin(0.1);
+  h_tot_true -> Draw("hist ERR");
+  h_tot_est  -> Draw("hist ERR same");
+  legend->Draw();
+
+  // Top canvas - distribution
+  // Bottom canvas - relative difference
+  c->cd();
+  pad2->Draw();
+  pad2->cd();
+  pad2->SetBottomMargin(0.25);
+  pad2->SetLeftMargin(0.1);
+  pad2->SetTopMargin(0.05);
+  h_diff_true->Draw("hist ERR");
+  c->SaveAs(output_file.c_str());
+  return 0 ;
+}

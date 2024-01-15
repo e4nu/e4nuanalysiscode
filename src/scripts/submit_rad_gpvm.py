544d14050d8c0fc93d3e894aa457f0a7c087570b
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-01-15 08:13:01 -0600

    adding change

--- src/scripts/submit_rad_gpvm.py
+++ src/scripts/submit_rad_gpvm.py
@@ -58,6 +58,7 @@ for x in gst_file_names:
     if os.path.exists(rad_dir+name_out_file+"_"+str(counter)+".sh"):
         os.remove(rad_dir+name_out_file+"_"+str(counter)+".sh")
 
+    genie_file=os.path.basename(gst_file_names[counter])
     script = open( rad_dir+name_out_file+"_"+str(counter)+".sh", 'w' ) 
     script.write("#!/bin/bash \n")
     script.write("source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups \n")
@@ -66,11 +67,11 @@ for x in gst_file_names:
     script.write("setup pdfsets v5_9_1b \n")
     script.write("setup gdb v8_1 \n")
     script.write("cd $CONDOR_DIR_INPUT \n")
-    script.write("ifdh cp -D "+gst_file_names[counter]+" $CONDOR_DIR_INPUT/genie.gst.root ;\n \n")
+    script.write("ifdh cp -D "+gst_file_names[counter]+" $CONDOR_DIR_INPUT/ ;\n \n")
     script.write("git clone "+opts.GIT_LOCATION+" -b "+opts.BRANCH+" ;\n")
     script.write("cd e4nuanalysiscode ; source e4nu_gpvm_env.sh ; make ;\n")
     #write main command
-    script.write("./process_radweights --input-gst-file $CONDOR_DIR_INPUT/genie.gst.root --output-gst-file $CONDOR_DIR_INPUT/rad_corr_"+str(counter)+".gst.root --true_EBeam "+str(opts.EnergyBeam)+" --target "+str(opts.TARGET)+" --rad-model "+opts.MODEL+" ; \n\n")
+    script.write("./process_radweights --input-gst-file $CONDOR_DIR_INPUT/"+genie_file+" --output-gst-file $CONDOR_DIR_INPUT/rad_corr_"+str(counter)+".gst.root --true-EBeam "+str(opts.EnergyBeam)+" --target "+str(opts.TARGET)+" --rad-model "+opts.MODEL+" ; \n\n")
     script.write("ifdh cp -D $CONDOR_DIR_INPUT/rad_corr_"+str(counter)+".gst.root "+rad_dir+" \n")
 
     grid.write("jobsub_submit  -n --memory=4GB --disk=4GB --expected-lifetime=4h  --OS=SL7 --mail_on_error file://"+rad_dir+name_out_file+"_"+str(counter)+".sh \n")

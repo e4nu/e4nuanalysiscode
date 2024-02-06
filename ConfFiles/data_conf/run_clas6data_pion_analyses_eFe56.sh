3f0bdba3472c302c379fcbcd19b04e679b775e52
jtenavidal <jtena@ifarm1901.jlab.org>
2024-02-06 11:39:59 -0500

    adding no background cases

--- ConfFiles/data_conf/run_clas6data_pion_analyses_eFe56.sh
+++ ConfFiles/data_conf/run_clas6data_pion_analyses_eFe56.sh
@@ -55,3 +55,21 @@ source e4nu_ifarm_env.csh
 # 1pip analysis
 ./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pipanalysis_eFe56_2GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pipanalysis_e_on_1000260560_2261MeV" --analysis-type "IsData" --bkg-mult 3
 ./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pipanalysis_eFe56_4GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pipanalysis_e_on_1000260560_4461MeV" --analysis-type "IsData" --bkg-mult 3
+
+
+# Multiplicity 0 - No subtraction
+# 1p1pim analysis
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1p1pimanalysis_eFe56_2GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1p1pimanalysis_e_on_1000260560_2261MeV" --analysis-type "IsData" --bkg-mult 0
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1p1pimanalysis_eFe56_4GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1p1pimanalysis_e_on_1000260560_4461MeV" --analysis-type "IsData" --bkg-mult 0
+
+# 1p1pip analysis
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1p1pipanalysis_eFe56_2GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1p1pipanalysis_e_on_1000260560_2261MeV" --analysis-type "IsData" --bkg-mult 0
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1p1pipanalysis_eFe56_4GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1p1pipanalysis_e_on_1000260560_4461MeV" --analysis-type "IsData" --bkg-mult 0
+
+# 1pim analysis
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pimanalysis_eFe56_2GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pimanalysis_e_on_1000260560_2261MeV" --analysis-type "IsData" --bkg-mult 0
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pimanalysis_eFe56_4GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pimanalysis_e_on_1000260560_4461MeV" --analysis-type "IsData" --bkg-mult 0
+
+# 1pip analysis
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pipanalysis_eFe56_2GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_2261_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pipanalysis_e_on_1000260560_2261MeV" --analysis-type "IsData" --bkg-mult 0
+./e4nuanalysis --conf-file ConfFiles/data_conf/clas6data_1pipanalysis_eFe56_4GeV.txt --root-file "/w/hallb-scshelf2102/clas/claseg2/apapadop/GetCharge_genie_filtered_data_e2a_ep_56Fe_4461_neutrino6_united4_radphot_test_100M.root" --output-file "/work/clas12/jtena/e4nuanalysis_1pipanalysis_e_on_1000260560_4461MeV" --analysis-type "IsData" --bkg-mult 0

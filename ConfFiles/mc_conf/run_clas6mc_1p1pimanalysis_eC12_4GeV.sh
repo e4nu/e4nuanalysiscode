a929ffef734875f341a7d10a76e9c5190dbb6928
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-02-06 09:42:13 -0600

    adding missing files and updating paths to MC

--- ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_4GeV.sh
+++ ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_4GeV.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
+path_mc_files="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024Generation/"
+path_output="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024AnalisedFiles/"
+path_xsec="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024Splines/"
 
-declare -a InputFiles=("/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/EventGeneration/G18_10a_00_000/G18_10a_Q2_08_e_1000060120_4461MeV_NoRad.gst.root"
-                       "/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/EventGeneration/G18_10b_00_000/G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad.gst.root"
-                       "/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/EventGeneration/G18_10a_00_000/G18_10a_Q2_08_NoFSI_e_1000060120_4461MeV_NoRad.gst.root")
+declare -a InputFiles=("G18_10a_Dipole_LFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11a_Dipole_CFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11a_Dipole_LFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11a_Dipole_RFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11b_Dipole_LFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11c_Dipole_CFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11c_Dipole_LFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11d_Dipole_CFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11d_Dipole_LFG_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11a_Dipole_LFG_NoFSI_Q2_08_4GeV_eCarbon.gst.root"
+    "GEM21_11a_Rarita_LFG_Q2_08_4GeV_eCarbon.gst.root")
 
-declare -a OutputFiles=("/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_Q2_08_e_on_1000060120_4461MeV_NoRad"
-                        "/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10b_Q2_08_e_on_1000060120_4461MeV_NoRad"
-			"/pnfs/genie/persistent/users/jtenavid/e4nu_files/AnalisedFiles/1p1pim/e4nuanalysis_1p1pimanalysis_G18_10a_NoFSI_Q2_08_e_on_1000060120_4461MeV_NoRad")
+declare -a OutputFiles=("e4nuanalysis_1p1pim_G18_10a_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11a_Dipole_CFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11a_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11a_Dipole_RFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11b_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11c_Dipole_CFG_Q2_08_4GeV_eCarbon.gst.root"
+    "e4nuanalysis_1p1pim_GEM21_11c_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11d_Dipole_CFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11d_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11a_Dipole_LFG_NoFSI_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11a_Rarita_LFG_Q2_08_4GeV_eCarbon_NoRad"
+)
 
-declare -a XSecFiles=("/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/Splines/G18_10a_00_000/G18_10a_00_000_eC12_Q2min_08_total_xsec.root"
-                      "/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/Splines/G18_10a_00_000/G18_10a_00_000_eC12_Q2min_08_total_xsec.root"
-		      "/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/Splines/G18_10a_00_000/G18_10a_00_000_eC12_Q2min_08_total_xsec.root")
+declare -a XSecFiles=("G18_10a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Dipole_Q2_08_eCarbon.root"
+		      "GEM21_11a_Rarita_Q2_08_eCarbon.root"
+)
+
+conf_file="ConfFiles/mc_conf/clas6mc_1p1pimanalysis_eC12_4GeV.txt"
 
 cd $E4NUANALYSIS
 
@@ -19,6 +50,6 @@ source e4nu_gpvm_env.sh
 number_inputs=${#InputFiles[@]}
 for (( i=0; i<${number_inputs}; i++ ));
 do
-  ./e4nuanalysis --conf-file ConfFiles/mc_conf/clas6mc_1p1pimanalysis_eC12_4GeV.txt --root-file ${InputFiles[$i]} --output-file ${OutputFiles[$i]} --analysis-type ComputeTrueAccCorr --xsec-file ${XSecFiles[$i]}
-  ./e4nuanalysis --conf-file ConfFiles/mc_conf/clas6mc_1p1pimanalysis_eC12_4GeV.txt --root-file ${InputFiles[$i]} --output-file ${OutputFiles[$i]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${XSecFiles[$i]}
+  ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[$i]} --output-file ${path_output}${OutputFiles[$i]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[$i]}
+  ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[$i]} --output-file ${path_output}${OutputFiles[$i]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${path_xsec}${XSecFiles[$i]}
 done

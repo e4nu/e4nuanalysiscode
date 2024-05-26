568e7ba1d81002e6afe254a2e8a07fec0856b4ad
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-05-26 12:32:34 -0500

    add division rad to ensure same number of events

--- ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_1GeV.sh
+++ ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_1GeV.sh
@@ -39,6 +39,8 @@ declare -a XSecFiles=("G18_10a_Dipole_Q2_01_eCarbon.root"
     "GEM21_11a_Dipole_Q2_01_eCarbon.root"
 )
 
+declare -a RadiatedFiles=("GEM21_11a_Dipole_LFG_Q2_01_1GeV_eCarbon_radiated.gst.root")
+
 conf_file="ConfFiles/mc_conf/clas6mc_1p1pimanalysis_eC12_1GeV.txt"
 
 cd $E4NUANALYSIS
@@ -54,3 +56,5 @@ done
 
 ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[0]} --output-file ${path_output}${OutputFiles[0]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --phi-shift 3
 ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[0]} --output-file ${path_output}${OutputFiles[0]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --phi-shift 3
+
+./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${RadiatedFiles[0]} --output-file ${path_output}"e4nuanalysis_1p1pim_GEM21_11a_Dipole_CFG_Q2_01_1GeV_eCarbon_Rad" --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --rad-corr true

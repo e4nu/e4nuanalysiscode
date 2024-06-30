99e79fa4e23b4a001b9ae415da16906ad130d33c
jtenavidal <jtenavidal@tauex.tau.ac.il>
2024-06-30 09:05:49 -0500

    adding new paths for 1pim case

--- ConfFiles/mc_conf/run_clas6mc_1pimanalysis_eC12_4GeV.sh
+++ ConfFiles/mc_conf/run_clas6mc_1pimanalysis_eC12_4GeV.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 path_mc_files="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024Generation/"
 path_output="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024AnalisedFiles/"
 path_xsec="/pnfs/genie/persistent/users/jtenavid/e4nu_files/GENIE_Files/2024Splines/"
@@ -42,12 +41,22 @@ declare -a XSecFiles=("G18_10a_Dipole_Q2_08_eCarbon.root"
 		      "GEM21_11a_Rarita_Q2_08_eCarbon.root"
 )
 
+declare -a RadiatedFiles=("GEM21_11a_Dipole_LFG_Q2_08_4GeV_eCarbon_radiated.gst.root")
+
 conf_file="ConfFiles/mc_conf/clas6mc_1pimanalysis_eC12_4GeV.txt"
+
 cd $E4NUANALYSIS
+
 source e4nu_gpvm_env.sh
+
 number_inputs=${#InputFiles[@]}
 for (( i=0; i<${number_inputs}; i++ ));
 do
   ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[$i]} --output-file ${path_output}${OutputFiles[$i]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[$i]}
   ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[$i]} --output-file ${path_output}${OutputFiles[$i]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${path_xsec}${XSecFiles[$i]}
 done
+
+./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[0]} --output-file ${path_output}${OutputFiles[0]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --phi-shift 3
+./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${InputFiles[0]} --output-file ${path_output}${OutputFiles[0]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --phi-shift 3
+
+./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${RadiatedFiles[0]} --output-file ${path_output}"e4nuanalysis_1pim_GEM21_11a_Dipole_LFG_Q2_08_4GeV_eCarbon_Rad" --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --rad-corr true

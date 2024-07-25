73354c7ebb9b4ee1954939c2d94127a3dad7cd56
Brittany Cohen <brittanykcohen96@gmail.com>
2024-07-25 18:25:36 +0300

    Added macro

--- src/plotting/ReadDataMacro.cxx
+++ src/plotting/ReadDataMacro.cxx
@@ -15,7 +15,6 @@
 int ReadDataMacro()
 {
     std::string data_file_name = "/storage/t3_data/tvjulia/e4nuanalysis_files/1pi_analysis/analised_data/e4nuanalysis_1pimanalysis_e_on_1000060120_4461MeV_4MaxBkgMult_clas6data.root";
-    // TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
 
     TFile *file_true_data = new TFile((data_file_name).c_str(), "ROOT");
     TTree *my_data_tree = (TTree *)file_true_data->Get("CLAS6Tree");
@@ -30,14 +29,16 @@ int ReadDataMacro()
 
     // OBSERVABLE DEFINITION:
     double RecoEvPion;
+    double pim_mom;
 
     my_data_tree->SetBranchAddress("RecoEvPion", &RecoEvPion);
+    my_data_tree->SetBranchAddress("pim_mom", &pim_mom);
 
     long NEntries = my_data_tree->GetEntries();
 
     for (int j = 0; j < NEntries; ++j)
     {
-        std::cout << RecoEvPion << std::endl;
+        std::cout << pim_mom << std::endl;
     }
 
     return 0;

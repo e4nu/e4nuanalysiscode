31493bf6e5aae9742f0ffd5844a10f75a7856dd7
Brittany Cohen <brittanykcohen96@gmail.com>
2024-07-25 18:14:29 +0300

    Added macro

--- src/plotting/ReadDataMacro.cxx
+++ src/plotting/ReadDataMacro.cxx
@@ -15,7 +15,7 @@
 int ReadDataMacro()
 {
     std::string data_file_name = "/storage/t3_data/tvjulia/e4nuanalysis_files/1pi_analysis/analised_data/e4nuanalysis_1pimanalysis_e_on_1000060120_4461MeV_4MaxBkgMult_clas6data.root";
-    TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+    // TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
 
     TFile *file_true_data = new TFile((data_file_name).c_str(), "ROOT");
     TTree *my_data_tree = (TTree *)file_true_data->Get("CLAS6Tree");
@@ -29,9 +29,9 @@ int ReadDataMacro()
         std::cout << " Tree does not exist " << std::endl;
 
     // OBSERVABLE DEFINITION:
-    double RecoEVPion;
+    double RecoEvPion;
 
-    my_gst_tree->SetBranchAddress("RecoEVPion", &RecoEVPion);
+    my_gst_tree->SetBranchAddress("RecoEvPion", &RecoEvPion);
 
     long NEntries = my_data_tree->GetEntries();
 

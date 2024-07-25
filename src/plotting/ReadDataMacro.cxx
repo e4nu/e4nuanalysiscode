f774bb2c9d1e94f23aac062fb99f31e35cd7866e
Brittany Cohen <brittanykcohen96@gmail.com>
2024-07-25 18:28:57 +0300

    Added macro

--- src/plotting/ReadDataMacro.cxx
+++ src/plotting/ReadDataMacro.cxx
@@ -17,28 +17,28 @@ int ReadDataMacro()
     std::string data_file_name = "/storage/t3_data/tvjulia/e4nuanalysis_files/1pi_analysis/analised_data/e4nuanalysis_1pimanalysis_e_on_1000060120_4461MeV_4MaxBkgMult_clas6data.root";
 
     TFile *file_true_data = new TFile((data_file_name).c_str(), "ROOT");
-    TTree *my_data_tree = (TTree *)file_true_data->Get("CLAS6Tree");
+    TTree *data_tree = (TTree *)file_true_data->Get("CLAS6Tree");
 
     if (!file_true_data)
     {
         std::cout << "ERROR: the " << file_true_data << " does not exist." << std::endl;
         return 0;
     }
-    if (!my_data_tree)
+    if (!data_tree)
         std::cout << " Tree does not exist " << std::endl;
 
     // OBSERVABLE DEFINITION:
     double RecoEvPion;
     double pim_mom;
 
-    my_data_tree->SetBranchAddress("RecoEvPion", &RecoEvPion);
-    my_data_tree->SetBranchAddress("pim_mom", &pim_mom);
+    data_tree->SetBranchAddress("RecoEvPion", &RecoEvPion);
+    data_tree->SetBranchAddress("pim_mom", &pim_mom);
 
-    long NEntries = my_data_tree->GetEntries();
+    long NEntries = data_tree->GetEntries();
 
     for (int j = 0; j < NEntries; ++j)
     {
-        std::cout << pim_mom << std::endl;
+        std::cout << NEntries, j, pim_mom << std::endl;
     }
 
     return 0;

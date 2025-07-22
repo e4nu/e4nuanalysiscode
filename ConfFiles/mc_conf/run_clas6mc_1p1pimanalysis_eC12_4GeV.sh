d57c320c367214cf7e5b7b00fd78442a86f7fd35
jtenavidal <jtenavidal@tauex.tau.ac.il>
2025-07-22 09:21:56 -0500

    updating config files to run also radiated with reco effects

--- ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_4GeV.sh
+++ ConfFiles/mc_conf/run_clas6mc_1p1pimanalysis_eC12_4GeV.sh
@@ -8,18 +8,21 @@ declare -a InputFiles=(
     "Rarita/Carbon/4GeV/"
     "Rarita/Carbon/NoFsi/4GeV/"
     "Dipole/Carbon/4GeV/"
+    "Geant4/Carbon/4GeV/"
 )
 
 declare -a OutputFiles=(
     "e4nuanalysis_1p1pim_GEM21_11a_Rarita_LFG_Q2_08_4GeV_eCarbon_NoRad"
     "e4nuanalysis_1p1pim_GEM21_11a_Rarita_LFG_Q2_08_4GeV_eCarbon_NoRad_NoFSI"
     "e4nuanalysis_1p1pim_GEM21_11a_Dipole_LFG_Q2_08_4GeV_eCarbon_NoRad"
+    "e4nuanalysis_1p1pim_GEM21_11d_Rarita_LFG_Q2_08_4GeV_eCarbon_NoRad"
 )
 
 declare -a XSecFiles=(
     "GEM21_11a_Rarita_Q2_08_C.gst.root"
     "GEM21_11a_Rarita_Q2_08_C.gst.root"
     "GEM21_11a_Dipole_Q2_08_C.gst.root"
+    "GEM21_11a_Rarita_Q2_08_C.gst.root"
 )
 
 ## Radiated input
@@ -46,7 +49,7 @@ done
 
 # Radiated
   ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${RadiatedFiles[0]} --output-file ${path_output}${OutputFilesRadiated[0]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --rad-corr true
-  ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${RadiatedFiles[0]} --output-file ${path_output}${OutputFilesRadiated[0]} --analysis-type ComputeTrueAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --rad-corr truereco
+  ./e4nuanalysis --conf-file ${conf_file} --root-file ${path_mc_files}${RadiatedFiles[0]} --output-file ${path_output}${OutputFilesRadiated[0]} --analysis-type ComputeTrueRecoAccCorr --xsec-file ${path_xsec}${XSecFiles[0]} --rad-corr true
 
 # For systematics
 
